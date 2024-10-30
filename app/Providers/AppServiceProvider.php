78fdbf4885613175d0b2e1c72320b95d5600b1e5
Konstantinos Tsouvalis <ktsouvalis@uop.gr>
2024-10-30 12:25:54 +0200

    install passport, passport:keys, install predis, interact through some testing routes with Redis server

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Laravel\Passport\Passport;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Passport::loadKeysFrom(storage_path());
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
+use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/redis-set/{key}/{value}', function ($key, $value) {
+    Redis::set($key, $value);
+    return "Key {$key} set to {$value}";
+});
+
+Route::get('/redis-get/{key}', function ($key) {
+    $value = Redis::get($key);
+    return $value ? "Key {$key} has value {$value}" : "Key {$key} not found";
+});
+
+Route::get('/check-redis', function () {
+    try {
+        return Redis::connection()->ping();
+    } 
+    catch (Exception $e) {
+        return $e->getMessage();
+    }
 });
